e96a2a3e2655110c0d68f9954dbac6042ac5252f
lansen <bluewoods.lan@gmail.com>
2022-09-26 15:37:30 +0800

    feat:链表和二叉树

--- 02.数据结构(二)/03.链表.js
+++ 02.数据结构(二)/03.链表.js
@@ -3,17 +3,18 @@ function ListNode(val) {
   this.next = null
 }
 
-const node1 = new ListNode(1)
-node1.next = new ListNode(2)
+const node = new ListNode(1)
+node.next = new ListNode(2)
+console.log(node)
 
 // 插入节点
 const node3 = new ListNode(3)
 
 // 在节点1和节点2之间插入节点3
-node3.next = node1.next()
-node1.next = node3
+node3.next = node.next
+node.next = node3
 
 // 删除node3
-node1.next = node3.next
+node.next = node3.next
 
 debugger
---
+++ 03.数据结构(三)/01.二叉树.js
@@ -0,0 +1,9 @@
+function TreeNode(val) {
+  this.val = val
+  this.left = this.right = null
+}
+
+const node = new TreeNode(1)
+console.log(node)
+
+debugger
